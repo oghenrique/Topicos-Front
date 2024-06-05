9cfb785675874833c9288682f107afb3c3d41d1a
oghenrique <santosguhenrique@gmail.com>
2024-06-04 23:59:04 -0300

    teste

---
+++ noticias.js
@@ -0,0 +1,143 @@
+const noticias = [
+    {
+      id: 1,
+      tipo: "carrossel",
+      imagem: "./img/futebol.png",
+      titulo: "Gol e confusão",
+      descricao: "Goleiro do Santos pediu atendimento, mas o atacante do América-MG roubou a bola e completou para o gol vazio.",
+      conteudo: `
+        <h1>Em jogo marcado por polêmica, América-MG vence o Santos e mantém invencibilidade</h1>
+        <p>João Paulo e Renato Marques protagonizaram o principal lance da partida. Aos 14 minutos do primeiro tempo, o goleiro do Santos disputou jogada dentro da grande área e, após sentir o pé esquerdo, pediu atendimento, mas o atacante do América-MG roubou a bola e completou para o gol vazio. O lance foi validado e colocou o Coelho em vantagem, mas gerou revolta nos santistas. Até o meio-campista Juninho, capitão do time mineiro, admitiu o erro dos donos da casa e afirmou que faltou fair play na jogada. Depois da partida, porém, Alê defendeu a atitude do atacante e falou que o time mineiro deu "chocolate".</p>
+        <p>O equilíbrio já era esperado no duelo entre o líder Santos FC e o América-MG, que estava invicto na competição. O começo da partida foi estudado, com poucas chances para os dois lados. A primeira chegada aconteceu aos cinco minutos, quando Gil aproveitou rebote em escanteio e chutou por cima da meta. </p>
+        <p>Aos 14 minutos, porém, o lance mais polêmico da Série B foi protagonizado por Renato Marques. O atacante do América-MG aproveitou uma séria lesão do goleiro João Paulo para roubar a bola e, ignorando completamente o fair play, empurrar para o fundo das redes, abrindo o placar para os donos da casa. </p>
+        <p>Revoltados com a atitude do atleta da equipe mineira, o elenco do Santos FC devolveu a bola na saída de jogo e aplaudiu de forma irônica a ausência de fair play no lance. João Paulo precisou ser substituído com suspeita de lesão tendínea no tornozelo esquerdo e Gabriel Brazão fez sua estreia com a camisa alvinegra.</p>
+      `,
+    },
+    {
+      id: 2,
+      tipo: "carrossel",
+      imagem: "./img/volei.png",
+      titulo: "Paola Egonu é alvo de racismo por jornalista na Itália",
+      descricao: "Considerada uma das principais jogadoras do mundo, a oposta Paola Egonu foi alvo de uma fala racista em um programa esportivo de uma TV da Itália, em 23/09/2022.",
+      conteudo: `
+        <h1>Paola Egonu é alvo de racismo por jornalista na Itália</h1>
+        <p>Considerada uma das principais jogadoras do mundo, a oposta Paola Egonu foi alvo de uma fala racista em um programa esportivo de uma TV da Itália, em 23/09/2022.</p>
+        <p>
+        Após a exibição de uma reportagem com imagens da campanha da seleção italiana feminina na Liga das Nações, a apresentadora Cristiana Buonamano, da Sky Sport, chamou Egonu de "macaco" ao comentar sobre a expectativa da Azzurra para o Mundial: "Você falou de dois macacos, mas sabe que eu acrescentaria um terceiro: Egonu. Busca, você, um adjetivo, diferente, original. Não sei ser original", disse a jornalista.</p>
+        <p>A atleta levou a Itália ao título da Liga das Nações em partida contra a seleção brasileira em julho de 2022, além de ter conquistado o prêmio de melhor jogadora do campeonato. Desde o início de sua carreira, Egonu tem sido vítima de ataques racistas.</p>
+      `,
+    },
+    {
+      id: 3,
+      tipo: "carrossel",
+      imagem: "./img/basquete-bebe.jpg",
+      titulo: "Depressão de Lucas Bebê",
+      descricao: "Lucas Bebê revela depressão após saída da NBA e relata deboche por doença: 'Começam a rir da sua cara'",
+      conteudo: `
+        <h1>Lucas Bebê revela depressão após saída da NBA e relata deboche por doença: 'Começam a rir da sua cara'</h1>
+        <p>Há um ano, Lucas Bebê estava jogando no Toronto Raptors, time que defende desde que foi selecionado com a 16ª escolha no geral pelo Boston Celtics, em 2013. Menos de 12 meses depois de deixar as quadras da NBA após não atrair interesse dos times na Liga, ele treina em São Paulo na esperança de um dia voltar ao melhor basquete do mundo.</p>
+        <p>Menos de 12 meses pode parecer pouco tempo, mas na vida de Lucas Bebê, de 26 anos, foi o suficiente para ele passar por momentos ruins, dar a volta por cima e se reencontrar. O pior desses momentos definitivamente foi um quadro de depressão sofrido pelo pivô de 2,13m desde o fim do ano passado.</p>
+        <p>Atualmente, Bebê está sem time depois de ter rompido com o Fuenlabrada, da Espanha, por causa da depressão. O atleta chegou a colocar em dúvida se queria continuar no esporte, mas com o tratamento da doença e ajuda dos familiares e amigos, se superou.</p>
+        <p>"Eu tenho responsabilidades, tenho uma filha para sustentar, uma família pra ajudar. Sei que pessoas que dependem de mim. Então isso me ajudou a não parar. Eu sou uma pessoa jovem com sonhos futuros e isso me fez despertar e ver que eu não poderia me render. Eu deveria tirar forças de onde quer que seja para poder voltar a jogar no meu máximo nível", disse o jogador, ao ESPN.com.br.</p>
+        <p>Muitos acham que a depressão é inatingível a atletas que ganham ou já ganharam milhões e atuam no mais alto escalão do seu esporte. Mas não é assim. Ainda mais na NBA, onde várias das estrelas da Liga já admitiram ter sofrido disso.</p>
+        <p>"Por isso que eu passei de comentar sobre as minhas frustrações e tristezas. Porque eu, durante esse período, falei com algumas pessoas que não deveria, mas achando que naquele momento iriam me ajudar e as respostas foram iguais às que você disse. Começam a rir da sua cara e debochar pelo fato de você ter uma certa vida financeira boa e status, acham que você é intocável, que você não é humano. Independente de termo status e dinheiro, todos nós somos seres humanos. Pode acontecer com todo mundo. Não é porque você é famoso que pode escapar disso", completou Lucas Bebê.</p>
+        <p>Uma das estrelas da NBA que admitiu já ter sofrido com a depressão foi DeMar DeRozan, companheiro de Bebê nos Raptors durante os quatro anos do brasileiro na liga.</p>
+        <p>"Todo mundo descobriu pela mídia. Talvez só o Lowry soubesse porque eles eram muito inseparáveis. Se você não é uma pessoa aberta como o DeMar não era. Muito gente boa, mas não era aberto que nem eu, que fala com todo mundo, é difícil você compartilhar algo desse tipo com as pessoas. Foi um espanto porque uma pessoa do nível físico, técnico e status dele a gente acaba achando que nunca vai acontecer isso. A gente nunca teve tempo de chegar e falar com ele porque ninguém sabia. E comigo também, já vinha acontecendo há um tempo, quando eu já estava fora da NBA. Lá para maio, junho, julho, já estava meio baleado. Setembro e outubro foi bem forte e aí decidi vir para o Brasil em dezembro", afirmou o pivô brasileiro.</p>
+        <p>Em sua carreira na NBA, Lucas Bebê teve médias de 3,2 pontos, 2,8 rebotes e 1 toco por partida. Ele se notabilizou principalmente por sua defesa.</p>
+        <p>Lucas Bebê não negou que, principalmente em seu tempo na Espanha, onde começou a carreira ainda como um adolescente antes de ir para a NBA, que exagerava nas festas e na bebedeira entre as partida. Hoje, afirma estar há 3 meses sem ingerir bebida alcóolica.</p>
+        <p>"Eu não sou vítima, não tenho medo de falar. Inclusive sou uma pessoa muito sincera e verdadeira que sempre assumi a responsabilidade. Inclusive sou muito parecido com um amigo meu, que é o Felipe Melo. Nós somos muito autênticos. A gente fala o que a gente pensa. Às vezes é errado, mas a gente fala a verdade. Eu nunca me escondi dos erros. Eu poderia ter tido mais oportunidades? Sim, mas talvez eu não tenha merecido. Porque todo mundo tem seu momento de sair, beber, mas eu passava dos limites às vezes. Mesmo com as coisas indo bem na minha vida. E ainda mais quando eu ia bem, não tinha ninguém que me desse um toque para parar", disse.</p>
+        <p>"Eu aprendi com a vida, quando eu fiquei desempregado agora, que eu vi que estava errado. Mesmo quando eu tomava decisões erradas, nunca fui uma pessoa ruim de grupo, pode perguntar em Toronto. Todo mundo sempre gostou de mim. Essa vida, um pouco dessa falta de ética de trabalho meio que me custou não estar na NBA hoje. Por isso as pessoas pegam no meu pé com relação à bebida, balada. Então eu aprendi que se você quer fazer isso, talvez na aposentadoria. Agora você tem que viver a vida regrada e com disciplina", completou.</p>
+        <p>"Eu saía muito. Na Espanha era um jogo por semana, diferente da NBA. Eu gostava de sair direto porque você novo, tinha dinheiro, não tinha limite. Então você ia encontrar noitada todo dia na Europa. Eu tinha 16, 17 anos, estava sozinho. Eu estava experimentando o mundo. Mas eu não sabia, não tinha essa ética de trabalho. Quando eu fui para os Estados Unidos que eu vi, aos 17, 18 anos, que uma criança de 15 já tinha uma ética de trabalho que eu nunca tive. Você vê que é mais cultural, então eu aprendi com a vida que eu estava errado".</p>
+        <p>Lucas Bebê treina em São Paulo e tem atraído sondagens de representantes da NBA. A ideia dele é estar pronto no meio do ano, quando começam os treinos das equipes, para fazer parte de uma pré-temporada na liga e regressar aos Estados Unidos. E será que ele acha que volta ao melhor basquete do mundo?</p>
+        <p>"Se você me fizesse essa pergunta há uns 3, 4 meses, eu teria dúvida. Hoje eu digo que sim. O que me faz ter a certeza de que eu vou voltar é a confiança, porque eu estou treinando, estou fazendo a coisa certa. Eu sei que hoje eu estou pronto psicologicamente para o desafio. E eu lembro que até quando eu estava na NBA eu não estava pronto. Hoje, fazendo as coisas que têm que ser feitas, vivendo a vida mais regrada de um profissional, me traz confiança", explicou.</p>
+        <p>"Eu fico sabendo que eu estou sendo sondado por alguns times. Meu trabalho é treinar e procurar performance dentro da quadra e o do meu empresário é buscar time. Eu procuro focar no meu trabalho e estar pronto para qualquer oportunidade que aparecer", disse o pivô.</p>
+        <p>Em 31 de agosto, começa o Mundial de basquete. O Brasil está no grupo F ao lado de Grécia, Montenegro e Nova Zelândia.</p>
+        <p>"Eu acho que eu poderia judar o Petrovic de alguma forma. Não só ele, mas a diretoria toda confia muito em mim. Isso me deixa muito feliz. Porém, há essa dificuldade de eu não ter clube. Eu não tive pré-temporada, foi na base de dor. Eles já estão numa crescente até chegar o Mundial. Vai me prejudica um pouco, porém se eu conseguir assinar com alguém em maio, junho, mesmo que seja para um training camp, vai me ajudar", finalizou.</p>
+      `,
+    },
+    {
+      id: 4,
+      tipo: "noticia",
+      imagem: "./img/wallace.webp",
+      titulo: "Caso Wallace",
+      subtitulo: "Conselho de Ética do COB aumenta suspensão do jogador para 5 anos",
+      descricao: "Conselho decide ampliar a suspensão após o jogador entrar em quadra na final da Superliga Masculina 22/23. Confederação também é punida com perda de repasse de verbas, e presidente é suspenso",
+      conteudo: `
+        <h1>CASO WALLACE: Conselho de Ética do COB aumenta suspensão do jogador para 5 anos</h1>
+        <p>Conselho decide ampliar a suspensão após o jogador entrar em quadra na final da Superliga Masculina 22/23 . Confederação também é punida com perda de repasse de verbas, e presidente é suspenso.</p>
+        <p>O Conselho de Ética do Comitê Olímpico do Brasil decidiu aumentar de 90 dias para cinco anos o tempo de suspensão de Wallace por post com enquete sobre tiro no presidente Luiz Inácio Lula da Silva. O oposto entrou em quadra na final da Superliga masculina, respaldado por uma liminar do Supremo Tribunal de Justiça Desportiva. O Conselho, porém, entende que a suspensão era soberana.</p>
+        <p>Além da suspensão de Wallace, o Conselho também anunciou uma punição à Confederação Brasileira de Vôlei. A CBV foi punida com a perda de repasses de verba por seis meses. Além disso, Radamés Lattari, atual presidente da CBV, foi suspenso.</p>
+      `,
+    },
+    {
+        id: 5,
+        tipo: "noticia",
+        imagem: "./img/handebol.webp",
+        titulo: "Racismo em torneio de handebol na Europa",
+        subtitulo: "Clube de handebol entra para a história ao conquistar o título mundial.",
+        descricao: "Os meninos do Herkules, clube de handebol, viveram sentimentos antagônicos na conquista do título do Mundial.",
+        conteudo: `
+            <h1>Brasileiros denunciam racismo em torneio de handebol na Europa</h1>
+            <p>Os meninos do Herkules, clube de handebol de Guarulhos, região metropolitana de São Paulo, viveram sentimentos antagônicos na conquista do título do Mundial de Clubes Partille Cup, na categoria sub-15 na grama. Para além da glória e emoção do título - se tornando o 1° clube da América a chegar no lugar mais alto do pódio -, a delegação do time também conviveu com ataques racistas e xenofóbicos durante outros campeonatos na Europa, em específico, no Dronninglund Cup, na Dinamarca.</p>
+            <p>Durante jogo contra a Suécia pelo sub-14, um menino grudou no nosso goleiro perto da baliza e começou a imitar macaco. E os que estavam assistindo viram e foram para cima dele, para falar com ele, chamá-lo de racista. Até o nosso técnico foi atrás e todo mundo ficou desnorteado.</p>
+            <p>O goleiro em questão era Gabriel Ferreira, um dos atletas ajudados financeiramente pelo clube e que disputou a primeira competição internacional.</p>
+            <p>- Até então estava tudo bem, o jogo estava bem disputado, porém foi chegando o final e estávamos ganhando. Tinha um pessoal que joga nesse time (os adversários), porém eles eram mais velhos, do sub-16. Eles estavam atrás do meu gol e me tirando a atenção, mas eu não estava dando bola. Até que um menino passou mais perto de mim disse “Monkey, monkey” e começou a imitar um macaco. Foi nessa hora que parei o jogo chamei o meu técnico. Fomos atrás, conversamos com o técnico do menino para e explicar a situação, porém não aconteceu nada. Ele foi embora, continuamos o jogo e conseguimos ganhar - disse Gabriel.</p>
+            <p>Apesar de tudo que aconteceu, Gabriel fez questão de comentar que realizou um sonho ao disputar, pela primeira vez, uma competição internacional e falou da importância do esporte na vida.</p>
+        `
+    }
+  ]
+  
+  function carregarNoticias() {
+    const carrosselContainer = document.querySelector('.carousel-inner')
+    const carrosselIndicators = document.querySelector('.carousel-indicators')
+    const noticiasContainer = document.querySelector('.noticias')
+  
+    noticias.forEach((noticia, index) => {
+      if (noticia.tipo === 'carrossel') {
+        const indicator = document.createElement('button')
+        indicator.type = 'button'
+        indicator.dataset.bsTarget = '#carouselExampleCaptions'
+        indicator.dataset.bsSlideTo = index
+        indicator.className = index === 0 ? 'active' : ''
+        indicator.setAttribute('aria-label', `Slide ${index + 1}`)
+        if (index === 0) indicator.setAttribute('aria-current', 'true')
+        carrosselIndicators.appendChild(indicator)
+  
+        const item = document.createElement('div')
+        item.className = `carousel-item ${index === 0 ? 'active' : ''}`
+        item.innerHTML = `
+          <img src="${noticia.imagem}" class="d-block w-100" alt="./img/no-image.png">
+          <div class="carousel-caption d-none d-md-block" style="background-color: rgba(0, 0, 0, 0.8)">
+            <h5>${noticia.titulo}</h5>
+            <p>${noticia.descricao}</p>
+          </div>
+        `
+        item.addEventListener('click', () => {
+          window.location.href = `pages/noticia.html?id=${noticia.id}`
+        })
+        carrosselContainer.appendChild(item)
+      } else if (noticia.tipo === 'noticia') {
+        const noticiaDiv = document.createElement('div')
+        noticiaDiv.className = 'noticia'
+        noticiaDiv.innerHTML = `
+          <div class="imagem">
+            <img src="${noticia.imagem}" alt="">
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${noticia.titulo}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">${noticia.subtitulo}</h6>
+              <p class="card-text">${noticia.descricao}</p>
+            </div>
+          </div>
+        `
+        noticiaDiv.addEventListener('click', () => {
+          window.location.href = `pages/noticia.html?id=${noticia.id}`
+        })
+        noticiasContainer.appendChild(noticiaDiv)
+      }
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', carregarNoticias)
+  
